4f6ab92f7f8e75ca46b51b8b5c7a589422e335b6
Carlaav <carla2002.av@gmail.com>
2022-03-18 17:59:52 +0100

    a

--- c.py
+++ c.py
@@ -1,1 +1,37 @@
+class Casa:
+    def __init__(self, lista_paredes):
+        self.paredes = lista_paredes
+    
+    
+    
+    def dimensionesCasa(self):
+        dimension = 0
+        for pared in self.paredes:
+            dimension = dimension + pared.dimension - pared.ventana.dimen
+        
+        return dimension
+    
+    
+class Pared:
+    def __init__(self, orientacion, dimension, dim_ventana):
+        self.orientacion = orientacion
+        self.dimension = dimension
+        self.ventana = Ventana(self, dim_ventana)
+        
 
+class Ventana:
+    def __init__(self, pared, ven_dim):
+        self.pared = pared
+        self.dimen = ven_dim
+            
+            
+
+pared_norte = Pared("NORTE", 100, 0.5) 
+pared_oeste = Pared("OESTE", 60, 1) 
+pared_sur = Pared("SUR", 70, 2) 
+pared_este = Pared("ESTE", 30, 1) 
+    
+
+
+casa = Casa([pared_norte, pared_oeste, pared_sur, pared_este]) 
+print(casa.dimensionesCasa())
